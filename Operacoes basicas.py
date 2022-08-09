9f2e825bd508fcb0e24990116a9c0b878f1241c9
Mylena <102628246+MylenaAndrade@users.noreply.github.com>
2022-08-09 17:39:32 -0300

    Add files via upload

---
+++ Operacoes basicas.py
@@ -0,0 +1,19 @@
+﻿# input a float value for variable a here
+a=float(input("Input a value: "))
+
+# input a float value for variable b here
+b=float(input("Input other value: "))
+
+# output the result of addition here
+print("The result of addition is : ", a + b)
+
+# output the result of subtraction here
+print("The result of subtraction is: ", a - b)
+
+# output the result of multiplication here
+print("The result of multiplication is: ", a * b)
+
+# output the result of division here
+print("The result of division is: ", round(a / b, 2 ) )
+
+print("\nThat's all, folks!")
