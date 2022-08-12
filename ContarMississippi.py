2f79b8cf0de992c9ba747b8138ab5f771147155c
Mylena <102628246+MylenaAndrade@users.noreply.github.com>
2022-08-12 14:39:25 -0300

    Add files via upload

---
+++ ContarMississippi.py
@@ -0,0 +1,10 @@
+﻿import time
+
+# Write a for loop that counts to five.
+    # Body of the loop - print the loop iteration number and the word "Mississippi".
+    # Body of the loop - use: time.sleep(1)
+
+for number in range(1,6):
+    print(number," Mississippi")
+    time.sleep(1)
+print("Ready or not, here I come!")
