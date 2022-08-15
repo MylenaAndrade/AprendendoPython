04c9025be71ee3a3f051e341347bce481c9ff0d3
Mylena <102628246+MylenaAndrade@users.noreply.github.com>
2022-08-15 18:24:22 -0300

    Add files via upload

---
+++ ComedorDeVogais2.0.py
@@ -0,0 +1,18 @@
+﻿word_without_vowels= " "
+user_word = input("Pick up a word: ")
+user_word = user_word.upper()
+
+for letter in user_word:
+    if letter == 'A':
+        continue
+    if letter == 'E':
+        continue
+    elif letter == 'I':
+        continue
+    elif letter == 'O':
+        continue
+    elif letter == 'U':
+        continue
+    else:
+        word_without_vowels = word_without_vowels + letter
+print(word_without_vowels + " ")
