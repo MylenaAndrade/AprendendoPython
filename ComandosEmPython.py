eddd5c2d877154be123d7c7892c38b6f3d7b8771
Mylena <102628246+MylenaAndrade@users.noreply.github.com>
2022-08-24 19:46:22 -0300

    Create ComandosEmPython.py

---
+++ ComandosEmPython.py
@@ -0,0 +1,13 @@
+print -> print("  ")
+input -> variavel=input("  ")
+end -> print("    ", end=" ")
+sep -> print("  " "   ", sep=" ")
++ ou ,  -> print("string" + variavel string) != ,
+0o ->print(0o123)
+0x -> print(0x123)
+\ -> \n \" \t
+** -> 2**3 = 2^3
+% -> 4%2= 0
+/ -> 4/2= 2.0
+* -> 1 * 2 = 2
+= -> atribuição
